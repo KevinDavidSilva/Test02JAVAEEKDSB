003b28d667831b51ed48964b9663fa2ab51c48a2
KevinSilva <kevinsilvadb503@gmail.com>
2024-08-07 11:16:36 -0600

    practica de relacion de tablas spring boot

---
+++ src/main/java/org/esfe/Test02JavaeekdsbApplication.java
@@ -0,0 +1,13 @@
+package org.esfe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test02JavaeekdsbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test02JavaeekdsbApplication.class, args);
+	}
+
+}
